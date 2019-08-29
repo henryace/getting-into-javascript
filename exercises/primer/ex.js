4d77de35340ecda4b819579ef7e027025ced6832
20180104-135423\grtert <grtert@gmail.com>
2019-08-29 13:52:36 +0800

    Update ex.js

--- exercises/primer/ex.js
+++ exercises/primer/ex.js
@@ -2,6 +2,19 @@
 
 // TODO: define printFavoriteBooks() function
 
+function addFavoriteBook(bookName){
+	if (!favoriteBooks.includes("Great")){
+		favoriteBooks.push(bookName);
+	}
+}
+
+function printFavoriteBooks(){
+	console.log(`Favorite Books: ${favoriteBooks.length}`);
+	for(let bookname of favoriteBooks){
+		console.log(bookname);
+	}		
+}
+
 var favoriteBooks = [];
 
 addFavoriteBook("A Song of Ice and Fire");
@@ -11,3 +24,12 @@ addFavoriteBook("Great Expectations");
 addFavoriteBook("You Don't Know JS");
 
 // TODO: print out favorite books
+
+printFavoriteBooks();
+
+// 2019/8/29
+// Memo:
+// 1.function 前贅詞
+// 2. clonsole.log 在``內使用變數 並加 $
+// 3.for loop 使用 let A of B 而非 A in B
+// 
